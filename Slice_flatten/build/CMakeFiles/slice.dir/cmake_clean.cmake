c2f567df4a8dcc1dcdf0e1dc482a3b0d7e5f1000
Charlie <86840060+CharlieLai0405@users.noreply.github.com>
2025-05-08 16:06:25 +0800

    Add files via upload

---
+++ Slice_flatten/build/CMakeFiles/slice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slice.dir/slice.cpp.o"
+  "CMakeFiles/slice.dir/slice.cpp.o.d"
+  "slice"
+  "slice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
